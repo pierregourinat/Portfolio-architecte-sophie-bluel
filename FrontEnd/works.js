f9e5f90f3831f691b54cba9cf3d42162a965ac97
pierre gourinat <pierregourinat@gmail.com>
2024-07-23 11:51:51 +0200

    Nettoyage du code, commentaires

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,8 +1,10 @@
+// Déclenchement de l'événement quand le document HTML est complètement chargé
 document.addEventListener("DOMContentLoaded", () => {
   //Definition  URL API
   const apiUrl = "http://localhost:5678/api/works";
   const cardContainer = document.getElementById("cardContainer");
 
+  // Fetch de l'API pour récupérer les ressources
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
@@ -10,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       return response.json();
     })
+    // Méthode map pour créer un tableau avec les résultats de l'appel d'une fonction
     .then((data) => {
       data.map((item) => {
         const figure = document.createElement("figure");
@@ -24,39 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         cardContainer.appendChild(figure);
       });
-      // console.log(data);
-      // outputElement.textContent = JSON.stringify(data, null, 2);
     })
+    // Catch les erreurs du code précédemment écrit
     .catch((error) => {
       console.error("Error :", error);
     });
 });
-
-// async function fetchWorks() {
-//   const r = await fetch("http://localhost:5678/api/works", {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//     },
-//   });
-//   if (r.ok === true) {
-//     return r.json();
-//   }
-//   throw new Error("Impossible de contacter le serveur");
-// }
-
-// fetchWorks().then((works) => console.log(users));
-
-// fetch("http://localhost:5678/api/works")
-//   .then((r) => r.json())
-//   .then((body) => console.log(body));
-
-// const content = await reponse.json();
-
-// export function ajoutAddWork() {
-//   const galleryElements = document.querySelectorAll(".gallery figure");
-
-//   for (let i = 0; i < galleryElements.length; i++) {
-//     piecesElements[i].addEventListener("click", async function (event) {});
-//   }
-// }
